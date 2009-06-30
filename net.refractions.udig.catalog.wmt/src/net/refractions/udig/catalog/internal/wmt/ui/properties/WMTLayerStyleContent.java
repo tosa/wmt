8182be8039be3b4c04173811498ddfe5119f75e2
Tobias Sauerwein <to.srwn@gmail.com>
2009-06-30 11:21:34 +0200

    use inheritance instead of copying..

--- net.refractions.udig.catalog.wmt/src/net/refractions/udig/catalog/internal/wmt/ui/properties/WMTLayerStyleContent.java
+++ net.refractions.udig.catalog.wmt/src/net/refractions/udig/catalog/internal/wmt/ui/properties/WMTLayerStyleContent.java
@@ -1,55 +1,20 @@
 package net.refractions.udig.catalog.internal.wmt.ui.properties;
 
-import java.awt.Color;
-import java.io.IOException;
-import java.net.URL;
+import net.refractions.udig.style.sld.editor.DialogSettingsStyleContent;
 
-import net.refractions.udig.catalog.IGeoResource;
-import net.refractions.udig.project.StyleContent;
-
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.ui.IMemento;
-
-// todo: see net.refractions.udig.style.sld.editor.DialogSettingsStyleContent
 
 /**
- * Persist an IMemento onto the style blackboard to save Style Dilog Settings between runs.
- * <p>
- * This is mostly used by the theme page right now.
- * @author Jesse Eichar
+ * Persist an IMemento onto the style blackboard to save 
+ * Style Dialog Settings between runs.
+ * 
+ * @see net.refractions.udig.style.sld.editor.DialogSettingsStyleContent
  */
-public class WMTLayerStyleContent extends StyleContent{
+public class WMTLayerStyleContent extends DialogSettingsStyleContent{
 
     public static final String EXTENSION_ID = "net.refractions.udig.catalog.internal.wmt.ui.properties.dialogSettings"; //$NON-NLS-1$
-    private static final String MEMENTO_KEY = "DialogSettings"; //$NON-NLS-1$
-
-    public WMTLayerStyleContent() {
-        super(EXTENSION_ID);
-    }
 
     @Override
-    public Object createDefaultStyle(IGeoResource resource, Color colour,
-            IProgressMonitor monitor) throws IOException {
-        return null;
-    }
-
-    @Override
-    public Class<? extends Object> getStyleClass() {
-        return IMemento.class;
-    }
-
-    @Override
-    public Object load(IMemento memento) {
-        return memento.getChild(MEMENTO_KEY);
-    }
-
-    @Override
-    public Object load(URL url, IProgressMonitor monitor) throws IOException {
-        return null;
-    }
-
-    @Override
-    public void save(IMemento memento, Object value) {
-        memento.createChild(MEMENTO_KEY).putMemento(((IMemento) value));
-    }
+    public String getId() {
+        return EXTENSION_ID;
+    }   
 }
